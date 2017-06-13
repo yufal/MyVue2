a80ea206f4be49e0d473f9804104d28779d2c691
loen Yu <yufaliang0629@163.com>
2017-06-13 18:37:15 +0800

    try to modify something

--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+/* 这里是引入vue文件 */
+/* 这里是引入同目录下的App.vue模块 */
+/* 这里是引入vue的路由 */
 import Vue from 'vue'
 import App from './App'
 import router from './router'
@@ -7,6 +10,10 @@ import router from './router'
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
+/* 定义作用范围就是index.html里的id为app的范围内 */
+/* 引入路由 */
+/* 给Vue实例初始一个App组件作为template 相当于默认组件 */
+/* 注册引入的组件App.vue */
 new Vue({
   el: '#app',
   router,
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,12 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import Hello from '@/components/Hello'
+import Vue from 'vue' /* 引用vue文件 */
+import Router from 'vue-router' /* 引用vue路由模块，并赋值给变量Router */
+import Hello from '@/components/Hello' /* 英文Hello.vue模版，并赋值给变量Hello,这里是“@”相当于“../” */
 
 Vue.use(Router)
-
 export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'Hello',
-      component: Hello
-    }
-  ]
+  routes: [{
+    path: '/',
+    name: 'Hello',
+    component: Hello
+  }]
 })
